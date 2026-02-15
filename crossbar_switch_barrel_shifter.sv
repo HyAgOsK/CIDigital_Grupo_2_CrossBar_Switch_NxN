cab7d93742038abc71bf05962f47156da22a5b25
HyAgOsK <hyagovieira@geb.inatel.br>
2026-02-15 12:15:23 -0300

    feat(barrelShifter): Implement generate-for barrel-shifter routing logic

--- crossbar_switch_barrel_shifter.sv
+++ crossbar_switch_barrel_shifter.sv
@@ -10,7 +10,15 @@ module crossbar_switch_barrel_shifter #(
 );
 
 	// 	TODO
-
 	// Lógica do crossbar switch usando barrel-shifter
+	genvar k;
+	generate
+	    for (k = 0; k < N; k = k + 1) begin: G_ROT
+			// out[k] recebe in[(k + shift) % N]
+			// shift=0 -> out = in
+			// shift=1 -> escolha de entrada vai para saída,  out[0]=in[1], out[1]=in[2] ..., out[N-1]=in[0]
+			assign out[k] = in[(k + shift) % N];
+		end
+	endgenerate
 
 endmodule
